1c4e816dbedfbccbb45c5655b6b90b383371bd3a
Christian Vajgel <christian.vajgel@al.infnet.edu.br>
2020-05-28 08:47:03 -0300

    New features added.

--- ListContact/Controllers/TodoController.cs
+++ ListContact/Controllers/TodoController.cs
@@ -9,37 +9,68 @@ namespace ListContact.Controllers
 {
     public class TodoController : Controller
     {
-        List<Todo> todos = new List<Todo>();
-        public IActionResult Index()
+       public static List<Todo> Todos { get; set; } = new List<Todo>();
+        public IActionResult Index(string? message)
         {
+            ViewBag.message = message;
+
             //List<Todo> todos = new List<Todo>();
 
-            todos.Add(new Todo()
-            {
-                Name = "TP1",
-                Finished = true
-            });
+            //todos.Add(new Todo()
+            //{
+            //    Name = "TP1",
+            //    Finished = true
+            //});
 
-            todos.Add(new Todo()
-            {
-                Name = "TP3",
-                Finished = false
-            });
+            //todos.Add(new Todo()
+            //{
+            //    Name = "TP3",
+            //    Finished = false
+            //});
 
-            todos.Add(new Todo()
-            {
-                Name = "AT",
-                Finished = false
-            });
+            //todos.Add(new Todo()
+            //{
+            //    Name = "AT",
+            //    Finished = false
+            //});
 
-            return View(todos);
+            return View(Todos);
         }    
         
         public IActionResult New()
         {
+            return View();
+        }
+
+        public IActionResult Edit([FromQuery] Guid id)
+        {
+            var todo = Todos.Where(x => x.Id == id).FirstOrDefault();
+            return View(todo);
+        }
 
 
-            return View(todos);
+        [HttpPost]
+        public IActionResult Save(Todo model) 
+        {
+            if (!ModelState.IsValid) { return View(); }
+            model.Id = Guid.NewGuid();
+            Todos.Add(model);
+            return RedirectToAction("Index", "Todo", new { message = "Task added."});
+        }
+
+        [HttpPost]
+        public IActionResult SaveEdit(Guid id, Todo model)
+        {
+            if (!ModelState.IsValid) { return View(); }
+
+            var todoEdit = Todos.Where(x => x.Id == id).FirstOrDefault();
+            todoEdit.Name = model.Name;
+            todoEdit.Finished = model.Finished;
+
+            Todos.Remove(todoEdit);
+            Todos.Add(todoEdit);
+
+            return RedirectToAction("Index", "Todo", new { message = "Task edited." });
         }
     }
 }
--- ListContact/Models/Todo.cs
+++ ListContact/Models/Todo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace ListContact.Models
 {
     public class Todo
     {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Every task should have a name.")]
         public String Name { get; set; }
         public Boolean Finished { get; set; }
     }
